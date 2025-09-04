19cd81761ed24b538ebf7446801b03d26d27d24d
root <root@srv986887.hstgr.cloud>
2025-09-04 01:27:48 +0000

    fix: corrigir case sensitivity no import UrgenciaDAO

--- BackEnd/testarConexao.js
+++ BackEnd/testarConexao.js
@@ -6,13 +6,14 @@ dotenv.config();
 async function testar() {
     try {
         const conexao = await mysql.createConnection({
-            host: 'localhost',
+            host: process.env.DB_HOST || '127.0.0.1',
+	    port: process.env.DB_PORT || 3306,
             user: process.env.DB_USER,
             password: process.env.DB_SENHA,
             database: process.env.DB_NOME,
         });
         console.log('✅ Conexão bem-sucedida com o banco!');
-        console.log('Conectando a', process.env.DB_USER, '@', process.env.DB_HOST ?? 'localhost', ':', process.env.DB_PORT ?? '3307');
+        console.log('Conectando a', process.env.DB_USER, '@', process.env.DB_HOST ?? '127.0.0.1', ':', process.env.DB_PORT ?? '3306');
 
         await conexao.end();
     } catch (erro) {
