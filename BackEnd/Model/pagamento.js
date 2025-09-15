c72607dc88891ed13edd9357ef64f64cedf8fd26
Carrielzada <offcarriel@gmail.com>
2025-09-14 23:58:07 -0300

    fix: corrigir erro de importação de arquivos

--- BackEnd/Model/pagamento.js
+++ BackEnd/Model/pagamento.js
@@ -1,39 +1,35 @@
-class PagamentoModel {
+import PagamentoDAO from "../Service/PagamentoDAO.js";
+
+export default class Pagamento {
     constructor(id, pagamento) {
         this.id = id;
-        this.pagamento = pagamento; 
+        this.pagamento = pagamento;
     }
 
     static validar(pagamento) {
         if (!pagamento.pagamento || pagamento.pagamento.trim() === "") {
-            throw new Error("O tipo de análise é obrigatório.");
+            throw new Error("O pagamento é obrigatório.");
         }
-
         return true;
     }
+
     async gravar() {
-        const PagamentoDAO = (await import("../Service/PagamentoDAO.js")).default;
         const pagamentoDAO = new PagamentoDAO();
         await pagamentoDAO.gravar(this);
     }
 
     async atualizar() {
-        const PagamentoDAO = (await import("../Service/PagamentoDAO.js")).default;
         const pagamentoDAO = new PagamentoDAO();
         await pagamentoDAO.atualizar(this);
     }
 
     async excluir() {
-        const PagamentoDAO = (await import("../Service/PagamentoDAO.js")).default;
         const pagamentoDAO = new PagamentoDAO();
         await pagamentoDAO.excluir(this);
     }
 
     async consultar(termo) {
-        const PagamentoDAO = (await import("../Service/PagamentoDAO.js")).default;
         const pagamentoDAO = new PagamentoDAO();
         return await pagamentoDAO.consultar(termo);
     }
 }
-
-export default PagamentoModel;
